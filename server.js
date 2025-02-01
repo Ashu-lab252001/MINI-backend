62fdd34748aeec9a57e5a80904f7fe3e1b5bb935
Ashutosh Koli <65846737+Ashu-lab252001@users.noreply.github.com>
2025-02-01 20:54:36 +0530

    final commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authroutes');  // Import auth routes
+
+dotenv.config();
+
+const app = express();
+const mongoURI = process.env.MONGO_URI;
+
+if (!mongoURI) {
+  console.error("❌ MongoDB URI is missing! Check your .env file.");
+  process.exit(1);
+}
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// MongoDB connection
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
+  console.log('MongoDB connected');
+}).catch((err) => {
+  console.log('MongoDB connection error:', err);
+});
+
+// Use auth routes
+app.use('/api/auth', authRoutes);
+
+// Server setup
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
